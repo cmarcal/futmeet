2937cbd22d9fc1933cbf76fb08d44b1603feaa98
cmarcal <cmarcal@users.noreply.github.com>
2026-02-18 18:10:51 -0300

    fix(a11y): import AxeResults from axe-core instead of jest-axe

--- src/test/a11y.ts
+++ src/test/a11y.ts
@@ -1,5 +1,5 @@
 import { axe, toHaveNoViolations } from 'jest-axe';
-import type { AxeResults } from 'jest-axe';
+import type { AxeResults } from 'axe-core';
 
 export { axe, toHaveNoViolations };
 export type { AxeResults };
