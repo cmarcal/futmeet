8fea9d8f3421c50fcff6dc6c0a2e9b72a5a72d07
cmarcal <cmarcal@users.noreply.github.com>
2026-02-20 20:55:15 -0300

    fix: lint and test for turbo monorepo

---
+++ apps/api/.eslintrc.cjs
@@ -0,0 +1,9 @@
+module.exports = {
+  root: true,
+  env: { node: true, es2022: true },
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 2022 },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['dist', 'node_modules'],
+};
